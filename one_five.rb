209537d70bd5abb8d59f5c8bd208a28b89d6763f
Elizabeth Wolensky <liz.wolensky@gmail.com>
2015-01-20 15:07:54 -0500

    one_five completed

--- one_five.rb
+++ one_five.rb
@@ -1,2 +1,15 @@
 #Write a method to replace all spaces in a string with ‘%20’.
-hey there pull request please
+
+def replaceSpaces(string)
+  return if string == nil
+  i = 0
+  while i < string.length
+    if string[i] == " "
+      string[i] = "%20"
+      i = i + 3
+    else
+      i = i + 1
+    end
+  end
+  string
+end
